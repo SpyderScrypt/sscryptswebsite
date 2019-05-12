ea9e9698379415413996ab62711974fbed3174e8
SpyderScrypt <spiderscrypt@gmail.com>
2019-05-13 04:25:21 +0530

    Updates

---
+++ precache-manifest.2cb17ee2c627ad6c14e0d6d0f55e4f46.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "8ede1036c3e60bf38b39",
+    "url": "/static/css/main.4ab2f341.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a3701f8eb835ef30005e",
+    "url": "/static/js/2.ff3730c2.chunk.js"
+  },
+  {
+    "revision": "8ede1036c3e60bf38b39",
+    "url": "/static/js/main.b1af8c2f.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a3701f8eb835ef30005e",
+    "url": "/static/css/2.186f1663.chunk.css"
+  },
+  {
+    "revision": "a05ace7124cf3871774f83765e3b1daa",
+    "url": "/index.html"
+  }
+];
